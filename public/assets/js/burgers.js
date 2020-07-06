d0874ef5a8e461ca5341b7736ee4e9ac0b70f951
Christian Gomez Martinez <gomez9289@gmail.com>
2020-07-06 17:10:09 -0400

    fixed buttons

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,8 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".box").on("submit", function(event) {
+$(document).ready(function() {
+  console.log('js file')
+  $(document).on("click" ,".save-btn", function(event) {
+    console.log('submit hit')
     event.preventDefault();
     // var id = $(this).data("id");
     var newBurger = {burger_name: $('#createBurger').val().trim()}
@@ -21,26 +23,7 @@ $(function() {
       }
     )
 
-  $(".devour-burger").on("click", function(event) {
-    // Make sure to preventDefault on a submit event.
-    // event.preventDefault();
-
-    var id = $(this).data("id");
-    var devoured = $(this).data("newdevour");
-    var newBurgerState = { devoured: true}
-
-    // Send the POST request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newBurgerState
-    }).then(
-      function() {
-        console.log(devoured);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+  
 
 //   $(".delete-cat").on("click", function(event) {
 //     var id = $(this).data("id");
@@ -58,4 +41,25 @@ $(function() {
 //   });
 // });
   })
+
+  $(document).on("click",".devour-burger", function(event) {
+    // Make sure to preventDefault on a submit event.
+    // event.preventDefault();
+
+    var id = $(this).data("id");
+    var devoured = $(this).data("newdevour");
+    var newBurgerState = { devoured: true}
+    console.log("about to update", id)
+    // Send the POST request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState
+    }).then(
+      function() {
+        console.log(devoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 })
