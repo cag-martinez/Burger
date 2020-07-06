b9cd601728976bb33c27dee6346455fbebca3cf6
Christian Gomez Martinez <gomez9289@gmail.com>
2020-07-06 17:37:08 -0400

    cleaned and formated the code

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,65 +1,36 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(document).ready(function() {
-  console.log('js file')
-  $(document).on("click" ,".save-btn", function(event) {
-    console.log('submit hit')
+$(document).ready(function () {
+  console.log("js file");
+  $(document).on("click", ".save-btn", function (event) {
+    console.log("submit hit");
     event.preventDefault();
-    // var id = $(this).data("id");
-    var newBurger = {burger_name: $('#createBurger').val().trim()}
 
-    // var newSleepState = {
-    //   sleepy: newSleep
-    // };
+    var newBurger = { burger_name: $("#createBurger").val().trim() };
 
     // Send the PUT request.
     $.ajax("/api/burgers", {
       type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("added a burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    )
+      data: newBurger,
+    }).then(function () {
+      console.log("added a burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 
-  
-
-//   $(".delete-cat").on("click", function(event) {
-//     var id = $(this).data("id");
-
-//     // Send the DELETE request.
-//     $.ajax("/api/cats/" + id, {
-//       type: "DELETE"
-//     }).then(
-//       function() {
-//         console.log("deleted cat", id);
-//         // Reload the page to get the updated list
-//         location.reload();
-//       }
-//     );
-//   });
-// });
-  })
-
-  $(document).on("click",".devour-burger", function(event) {
-    // Make sure to preventDefault on a submit event.
-    // event.preventDefault();
-
+  $(document).on("click", ".devour-burger", function (event) {
     var id = $(this).data("id");
     var devoured = $(this).data("newdevour");
-    var newBurgerState = { devoured: true}
-    console.log("about to update", id)
+    var newBurgerState = { devoured: true };
+    console.log("about to update", id);
     // Send the POST request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newBurgerState
-    }).then(
-      function() {
-        console.log(devoured);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+      data: newBurgerState,
+    }).then(function () {
+      console.log(devoured);
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
-})
+});
